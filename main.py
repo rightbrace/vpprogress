4f8bca5de69b8a8e1dab4fa44d73ba3152b6e4bb
Josef Duchesne <josef@cnmississaugawest.ca>
2021-06-15 14:54:53 -0400

    Day 2.2

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ WINDOW_WIDTH = 1100
 WINDOW_HEIGHT = 600 
 WINDOW_RES = (WINDOW_WIDTH, WINDOW_HEIGHT) 
 
+WIDTH = 100 
+HEIGHT = 100 
+
+WHITE = (255, 255, 255) 
+
 GAME_WINDOW = display.set_mode(WINDOW_RES) 
 display.set_caption('Attack of the Vampire Pizzas!') 
 
@@ -15,7 +20,12 @@ BACKGROUND = transform.scale(background_surf, WINDOW_RES)
 
 pizza_img = image.load('vampire.png') 
 pizza_surf = Surface.convert_alpha(pizza_img) 
-VAMPIRE_PIZZA = transform.scale(pizza_surf, (100, 100)) 
+VAMPIRE_PIZZA = transform.scale(pizza_surf, (WIDTH, HEIGHT)) 
+
+tile_color = WHITE 
+for row in range(6): 
+    for column in range(11): 
+        draw.rect(BACKGROUND, tile_color, (WIDTH*column, HEIGHT*row, WIDTH, HEIGHT), 1) 
 
 GAME_WINDOW.blit(BACKGROUND, (0, 0)) 
 GAME_WINDOW.blit(VAMPIRE_PIZZA, (900, 400)) 
